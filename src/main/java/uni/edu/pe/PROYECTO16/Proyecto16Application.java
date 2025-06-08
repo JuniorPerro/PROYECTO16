5c06f1d2322ee739b72c86d7284da6e5e7e57985
JuniorPerro <biritoproUSA@gmail.com>
2025-06-08 01:17:56 -0500

    Primer commit del proyecto PROYECTO16

---
+++ src/main/java/uni/edu/pe/PROYECTO16/Proyecto16Application.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.PROYECTO16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto16Application.class, args);
+	}
+
+}
---
+++ src/test/java/uni/edu/pe/PROYECTO16/Proyecto16ApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.PROYECTO16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
